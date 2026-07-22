482cc071551cce7ba2c8209248f45b1a9d77198f
JHB1\PreciousZ <preciousz@cappayments.co.za>
2026-07-23 01:54:36 +0200

    Added User, Role and Permission domain entities

---
+++ src/MultiPlatform.Domain/Entities/Role.cs
@@ -0,0 +1,20 @@
+﻿using MultiPlatform.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiPlatform.Domain.Entities
+{
+    public class Role : AuditableEntity
+    {
+        public Guid TenantId { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public Tenant Tenant { get; set; } = null!;
+    }
+}
---
+++ src/MultiPlatform.Domain/Entities/User.cs
@@ -0,0 +1,26 @@
+﻿using MultiPlatform.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiPlatform.Domain.Entities
+{
+    public class User : AuditableEntity
+    {
+        public Guid TenantId { get; set; }
+
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string PasswordHash { get; set; } = string.Empty;
+
+        public bool IsActive { get; set; } = true;
+
+        public Tenant Tenant { get; set; } = null!;
+    }
+}
