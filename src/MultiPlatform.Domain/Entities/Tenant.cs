caa8efa0aa95b12846ff5e0cf7515aeaba5d96ee
JHB1\PreciousZ <preciousz@cappayments.co.za>
2026-07-23 01:28:07 +0200

    Phase 1: Add Domain foundation and Tenant entity

---
+++ src/MultiPlatform.Domain/Entities/Tenant.cs
@@ -0,0 +1,18 @@
+﻿using MultiPlatform.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiPlatform.Domain.Entities
+{
+    public class Tenant :AuditableEntity
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string Identifier { get; set; } = string.Empty;
+
+        public bool IsActive { get; set; } = true;
+    }
+}
